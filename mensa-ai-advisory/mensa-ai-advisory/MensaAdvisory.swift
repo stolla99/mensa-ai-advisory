67d6ac6a2b562f42be9d83c6cc6f901864751903
Arne <stolla@rhrk.uni-kl.de>
2024-05-30 01:00:16 +0200

    Initial scene and gitignore

---
+++ mensa-ai-advisory/mensa-ai-advisory/MensaAdvisory.swift
@@ -0,0 +1,17 @@
+//
+//  mensa_ai_advisoryApp.swift
+//  mensa-ai-advisory
+//
+//  Created by Arne Stoll on 30.05.24.
+//
+
+import SwiftUI
+
+@main
+struct MensaAdvisory: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
